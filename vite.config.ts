1954bfb79b9446b7432e944250218451191f807d
vlofgrens <victor.sattamini@cloudwalk.io>
2025-05-14 10:47:39 -0300

    fixing api url

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://mortality-flask.onrender.com',
         changeOrigin: true,
       }
     },
